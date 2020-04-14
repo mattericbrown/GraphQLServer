7fc3ff1ccaac934ad9385fad7066c76240796c11
mattericbrown <matt.eric.brown@gmail.com>
2020-04-14 16:49:43 -0700

    server port for Heroku

--- src/index.ts
+++ src/index.ts
@@ -64,7 +64,7 @@ const main = async () => {
 
   apolloServer.applyMiddleware({ app, cors: false });
 
-  app.listen(4000, () => {
+  app.listen( {port: process.env.PORT || 4000 }, () => {
     console.log("server started on http://localhost:4000/graphql");
   })
 };
