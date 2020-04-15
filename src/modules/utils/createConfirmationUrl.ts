853ea60198b5c3c8b1765c05d6db2fa82167e540
mattericbrown <matt.eric.brown@gmail.com>
2020-04-15 12:05:27 -0700

    configure for nodemailer to send proper email

--- src/modules/utils/createConfirmationUrl.ts
+++ src/modules/utils/createConfirmationUrl.ts
@@ -6,5 +6,5 @@ export const createConfirmationUrl = async (userId: number) => {
   const token = v4();
   await redis.set(confirmUserPrefix + token, userId, "ex", 60*60*24);
 
-  return `http://localhost:3000/user/confirm/${token}`;
+  return `https://developerquiz.herokuapp.com/user/confirm/${token}`;
 }
--- src/modules/utils/sendEmail.ts
+++ src/modules/utils/sendEmail.ts
@@ -1,23 +1,20 @@
 import nodemailer from "nodemailer";
 
 export async function sendEmail(email: string, url: string) {
-  const account = await nodemailer.createTestAccount();
 
   const transporter = nodemailer.createTransport({
-    host: "smtp.ethereal.email",
-    port: 587,
-    secure: false, // true for 465, false for other ports
+    service: 'gmail',
     auth: {
-      user: account.user, // generated ethereal user
-      pass: account.pass, // generated ethereal password
+      user: "developers.quiz.app@gmail.com", // generated ethereal user
+      pass: 'moosemaloose', // generated ethereal password
     },
   });
 
   const mailOptions = {
-    from: '"Fred Foo 👻" <foo@example.com>', // sender address
+    from: '"Developers Quiz" <developers.quiz.app@gmail.com>', // sender address
     to: email, // list of receivers
-    subject: "Hello ✔", // Subject line
-    text: "Hello world?", // plain text body
+    subject: "Welcome to Developers Quiz App", // Subject line
+    text: "please confirm your account!", // plain text body
     html: `<a href="${url}">${url}</a>`, // html body
   };
 
