8bea4a3842c9652864d5e20307ee1ffa72d6cb72
mattericbrown <matt.eric.brown@gmail.com>
2020-04-24 13:50:05 -0700

    check for MeResolver

--- src/modules/user/Me.ts
+++ src/modules/user/Me.ts
@@ -7,8 +7,10 @@ export class MeResolver {
   @Query(() => User, { nullable: true })
   async me(@Ctx() ctx: MyContext): Promise<User | undefined> {
     if (!ctx.req.session!.userId) {
+      console.log("req.session userId not found");
       return undefined;
     }
+    console.log("user found!")
     return User.findOne(ctx.req.session!.userId)
   }
 }
