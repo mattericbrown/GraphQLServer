114abeee9c4857e161fa069154305c2def0f0625
im-mattbrown <matt.eric.brown@gmail.com>
2021-07-02 00:40:28 -0700

    redisURL

--- src/redis.ts
+++ src/redis.ts
@@ -1,6 +1,6 @@
 import Redis from 'ioredis';
 
-export const redis = new Redis(process.env.REDIS_TLS_URL);
+export const redis = new Redis(process.env.REDIS_URL);
 
 redis.on("error", (error: any) => {
   console.log("Redis connection error", error);
